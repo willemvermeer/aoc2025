8d1cc305da58c856d8692452a63e0889435ac6aa
Willem Vermeer <willem.vermeer@gmail.com>
2025-12-02 08:04:29 +0100

    day 1 done

--- AOC2025/src/main/scala/day1/main.scala
+++ AOC2025/src/main/scala/day1/main.scala
@@ -5,6 +5,19 @@ import scala.io.Source
 enum Direction:
   case Left, Right
 
+case class Dial(state: Int, passedZero: Int):
+  def rotateLeft =
+    if (state == 0) Dial(99, passedZero)
+    else Dial(state - 1, if (state == 1) passedZero + 1 else passedZero)
+  def rotateRight =
+    if (state == 99) Dial(0, passedZero + 1)
+    else Dial(state + 1, passedZero)
+  def apply(r: Rotation) =
+    (1 to r.degrees).foldLeft[Dial](this) { case (acc, elt) =>
+      if (r.d == Direction.Right) acc.rotateRight
+      else acc.rotateLeft
+    }
+
 def parseInput(input: Seq[String]) =
   input.map(line =>
     val degrees = line.drop(1).toInt
@@ -15,36 +28,16 @@ def parseInput(input: Seq[String]) =
 
 @main
 def main(): Unit =
-  val input = readFile("src/main/scala/day1/example")
+  val input = readFile("src/main/scala/day1/input")
   val rotations: Seq[Rotation] = parseInput(input)
-  val start = 50
-  rotations.foreach(println)
-  val vals = rotations.foldLeft[Seq[Int]](Seq(start)) { case (acc,elt) =>
-    acc :+ elt.apply(acc.reverse.head)
+  val dial = Dial(50, 0)
+  val dials = rotations.foldLeft[Seq[Dial]](Seq(dial)) { case (acc, elt) =>
+    acc :+ acc.reverse.head.apply(elt)
   }
-  println(vals.count(_ == 0))
-  val vals2 = rotations.foldLeft[Seq[(Int, Int)]](Seq((start, 0))) { case (acc,elt) =>
-    acc :+ elt.apply2(acc.reverse.head._1)
-  }
-  println(vals2.count(_._1 ==0))
-    println(vals2.map(_._2).sum)
+  println(dials.count(_.state == 0))
+  println(dials.map(_.passedZero).reverse.head)
 
-
-case class Rotation(d: Direction, degrees: Int):
-  def apply(start: Int) =
-    val intermediate = d match {
-      case Direction.Left => start - degrees
-      case Direction.Right => start + degrees
-    }
-    (100 + intermediate) % 100
-  def apply2(start: Int): (Int,Int) =
-    d match {
-      case Direction.Left =>
-        val dist = start - degrees
-        ((start - degrees) % 100, (start - degrees) / 100)
-      case Direction.Right =>
-        ((100 + start + degrees) % 100, (start + degrees) / 100)
-    }
+case class Rotation(d: Direction, degrees: Int)
 
 def readFile(filename: String): Seq[String] =
   val bufferedSource = Source.fromFile(filename)
